8f53f74a85ab07b80ef534d88742c258afe8ee10
Bogdan Markov <bogdick@yandex.ru>
2022-04-13 22:37:35 +0200

    [Advent of Code 2021, 18] - Add Advent18Test (failing).

--- src/main/kotlin/advent18/advent18.kt
+++ src/main/kotlin/advent18/advent18.kt
@@ -3,3 +3,11 @@ package advent18
 fun main() {
 
 }
+
+fun findSum(inputFile: String): SnailfishNumber {
+    return SnailfishNumber(0)
+}
+
+fun findMagnitude(inputFile: String): Int {
+    return 0
+}
---
+++ src/test/kotlin/advent18/Advent18Test.kt
@@ -0,0 +1,35 @@
+package advent18
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Advent18Test {
+
+    private val inputFileAndSum = mapOf(
+        "input1" to "[[[[1,1],[2,2]],[3,3]],[4,4]]",
+        "input2" to "[[[[3,0],[5,3]],[4,4]],[5,5]]",
+        "input3" to "[[[[5,0],[7,4]],[5,5]],[6,6]]",
+        "input4" to "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]",
+        "input5" to "[[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]",
+    )
+
+    private val inputFileAndMagnitude = mapOf(
+        "input5" to 4140,
+    )
+
+    @Test
+    fun findSum() {
+        for ((inputFile, sum) in inputFileAndSum) {
+            val actualSum = findSum(inputFile)
+            assertEquals(expected = sum, actual = actualSum.toString(), message = "Sums are different.")
+        }
+    }
+
+    @Test
+    fun findMagnitude() {
+        for ((inputFile, magnitude) in inputFileAndMagnitude) {
+            val actualMagnitude = findMagnitude(inputFile)
+            assertEquals(expected = magnitude, actual = actualMagnitude, message = "Magnitudes are different.")
+        }
+    }
+}
